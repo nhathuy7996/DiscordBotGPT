dfbf2a91a612a13b634bebe44fdae3ea9930e570
nhathuy7996 <nhat.huy.7996@gmail.com>
2023-06-06 11:47:50 +0700

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+from app.discord_bot.discord_api import client, discord_token
+ 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    client.run(discord_token)
+ 
